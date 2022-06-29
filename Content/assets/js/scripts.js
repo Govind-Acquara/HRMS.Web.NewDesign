3153770c7f0a21e48ba088627a3f450a3608ef4f
arpit.sharma14 <arpit.sharma@acquara.com>
2022-06-29 16:30:16 +0530

    Sidebar import

--- Content/assets/js/scripts.js
+++ Content/assets/js/scripts.js
@@ -7,6 +7,31 @@
   PLACE HERE YOUR OWN JAVASCRIPT CODE IF NEEDED
   WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR JAVASCRIPT CODE PLEASE CONSIDER WRITING YOUR SCRIPT HERE.  */
 
+          // Sidebar Javascript start
+
+          const sidebar = document.querySelector("#sidebar-toggle");
+          const overlay = document.querySelector(".overlay-bg");
+          const sidebarContent = document.querySelector("#sidebar-content");
+  
+          sidebar.addEventListener("click", () =>{
+              sidebar.classList.toggle("side-active");
+              sidebarContent.classList.toggle("side-active");
+              overlay.classList.toggle("show");
+          })
+  
+          document.querySelector(".overlay-bg").addEventListener("click", () => {
+              sidebar.classList.remove("side-active");
+              sidebarContent.classList.remove("side-active");
+              overlay.classList.remove("show");
+          })
+  
+          document.querySelector(".CloseBTN").addEventListener("click", () => {
+              sidebar.classList.remove("side-active");
+              sidebarContent.classList.remove("side-active");
+              overlay.classList.remove("show");
+          })
+  
+          // Sidebar Javascript end
 
   
 
